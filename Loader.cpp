c1b1a1436796e5ddb7b699383f92130b121de810
kevin729 <kevin@kevsbox.com>
2022-07-19 01:04:49 +0100

    First commit

---
+++ Loader.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+void Load() 
+{
+	const int vertices[] = {0,1,0, -1,-1,0, 1,-1,0}; //verticesVBO
+	const int textureCo[] = {0.5,1, 0,0, 1,0}; //textureCoVBO
+	const int normals[] = {0,0,1, 0,0,1, 0,0,1}; //normalsVBO
+}
